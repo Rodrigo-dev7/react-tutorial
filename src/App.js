bbe8b2f8c2a0b3d28199b22d49529109107a0ba5
rodrigo <fcorodrigo.ti@gmail.com>
2020-02-16 19:18:25 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import Table from './Table'
+import Form from './Form'
+
+
+
+class App extends Component {
+  state = {
+    characters: [],
+  }
+
+
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character]
+    })
+  }
+
+
+
+ render() {
+   const { characters } = this.state
+
+   return (
+     <div className="container">
+       <h1>React Tutorial</h1>
+       <p>Adicione um caractere com um nome e um trabalho à tabela. </p>
+       <Table
+       characterData={characters}
+       removeCharacter={this.removeCharacter}
+       />
+      <h3>Adicionar Novo</h3>
+      <Form handleSubmit={this.handleSubmit} />
+
+     </div>
+   )
+ }
+
+}
+
+export default App
